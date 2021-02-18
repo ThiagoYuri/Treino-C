0b5bf0b6881ed46a960efccdc0b225b000b49c23
ThiagoYuri <thiagoyuri0109@gmail.com>
2021-02-18 08:47:19 -0300

    Projetos em C

---
+++ ImprimirBase16/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("The value of twenty-one is: %d \n", 0x015);
+	printf("The value of twenty-two is: %d \n", 0x016);
+	printf("The value of sixty-two is: %d \n", 0x3E);
+	printf("The value of seventy-four is: %d \n", 0x4A);
+	
+
+	return 0;
+}

a6236b2e123932a131cebe88db43767aaae9baf5
ThiagoYuri <thiagoyuri0109@gmail.com>
2021-02-18 08:11:35 -0300

    Primeiros projetos treino para linguagem de programação C

---
+++ ImprimirNaTelaNumeroInteiro/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("The value of five is:\n%d", 5 );
+	return 0;
+}

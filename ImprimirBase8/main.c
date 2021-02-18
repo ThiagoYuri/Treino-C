4d2675569fe1def4d2a36dffa806c1bf7378aaa5
ThiagoYuri <thiagoyuri0109@gmail.com>
2021-02-18 08:37:06 -0300

    Projetos em C

---
+++ ImprimirBase8/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("The value of nine is: %d \n", 0x9);
+	printf("The value of ten is: %d \n", 0xA);
+	
+	return 0;
+}
---
+++ ImprimirValor7e8/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("The value of seven is: %d \n", 7 );
+	printf("The value of eight is: %d \n", (8000 - 7992));
+	return 0;
+}
